0e73c155c9056fb76efb92d1f2034204a64823e6
Davey Holler <daveyholler@hey.com>
2022-01-13 15:18:13 -0800

    Initial commit

---
+++ lua/user/alpha.lua
@@ -0,0 +1,50 @@
+local status_ok, alpha = pcall(require, "alpha")
+if not status_ok then
+	return
+end
+
+local dashboard = require("alpha.themes.dashboard")
+dashboard.section.header.val = {
+[[ ███████ ███    ██  █████  ██   ██ ███    ██  ██████ ███████ ██ ]],
+[[ ██      ████   ██ ██   ██ ██   ██ ████   ██ ██      ██      ██ ]],
+[[ █████   ██ ██  ██ ███████ ███████ ██ ██  ██ ██      █████   ██ ]],
+[[ ██      ██  ██ ██ ██   ██ ██   ██ ██  ██ ██ ██      ██         ]],
+[[ ███████ ██   ████ ██   ██ ██   ██ ██   ████  ██████ ███████ ██ ]],
+}
+dashboard.section.buttons.val = {
+	dashboard.button("f", "  Find file", ":Telescope find_files <CR>"),
+	dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
+	dashboard.button("p", "  Find project", ":Telescope projects <CR>"),
+	dashboard.button("r", "  Recently used files", ":Telescope oldfiles <CR>"),
+	dashboard.button("t", "  Find text", ":Telescope live_grep <CR>"),
+	dashboard.button("c", "  Configuration", ":e ~/.config/nvim/init.lua <CR>"),
+	dashboard.button("q", "  Quit Neovim", ":qa<CR>"),
+}
+
+local function footer()
+-- NOTE: requires the fortune-mod package to work
+	-- local handle = io.popen("fortune")
+	-- local fortune = handle:read("*a")
+	-- handle:close()
+	-- return fortune
+  messages = {
+    "I don't know what I'm doing...",
+    "4, 8, 15, 16, 23, 42",
+    "Recommissioning in progress",
+    "Can you monitor my allocation tables for a few minutes?",
+    "I thought hub 5 was out for a reboot.",
+    "Someone backed into by system through a subnet.",
+    "Isolate the node and dump it on the other side of the router."
+  }
+	return messages[math.random(#messages)] 
+end
+
+dashboard.section.footer.val = footer()
+
+dashboard.section.footer.opts.hl = "Type"
+dashboard.section.header.opts.hl = "Include"
+dashboard.section.buttons.opts.hl = "Keyword"
+
+dashboard.opts.opts.noautocmd = true
+-- vim.cmd([[autocmd User AlphaReady echo 'ready']])
+alpha.setup(dashboard.opts)
