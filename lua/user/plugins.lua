abeca80d31f98b5c5c15448e28d43cd0dcfa7162
Davey Holler <daveyholler@hey.com>
2022-01-19 08:40:56 -0800

    plugin updates.

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -82,10 +82,12 @@ return packer.startup(function(use)
   use "hrsh7th/cmp-cmdline" -- cmdline completions
   use "saadparwaiz1/cmp_luasnip" -- snippet completions
   use "hrsh7th/cmp-nvim-lsp"
+  use "github/copilot.vim"
 
   -- snippets
   use "L3MON4D3/LuaSnip" --snippet engine
   use "rafamadriz/friendly-snippets" -- a bunch of snippets to use
+  use "mattn/emmet-vim"
 
   -- LSP
   use "neovim/nvim-lspconfig" -- enable LSP
